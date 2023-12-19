d6fee04a22d4cc2ce256ad93518834ef6537cf6a
Anes Marzuki <amarzuki@yellowsys.fr>
2023-12-19 16:17:56 +0100

    Handle req body

--- src/pizzas/pizzas.controller.ts
+++ src/pizzas/pizzas.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('pizzas')
 export class PizzasController {
   @Get()
+  // findAll(@Res() res) { res.status(200).send('hello world'); }
   findAll() {
     return 'Returns all pizzas';
   }
@@ -10,5 +11,10 @@ export class PizzasController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This actions returns ${id}`;
+  }
+
+  @Post()
+  create(@Body() body): string {
+    return body;
   }
 }
